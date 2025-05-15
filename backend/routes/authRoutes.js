bb23f1797ee229aade88fb4d49a3a783b34de252
Abhi Singh <as78397423@gmail.com>
2025-05-15 06:12:33 +0000

    first commit

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -9,12 +9,15 @@ const router = express.Router();
 // Register
 router.post('/register', async (req, res) => {
   const { name, email, password, role } = req.body;
+  console.log(name, email, password, role);
   try {
     const existing = await User.findOne({ email });
     if (existing) return res.status(400).json({ message: 'Email already in use' });
 
     const hashed = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ name, email, password: hashed, role });
+    const defaultRole = role || "user";
+    const newUser = await User.create({ name, email, password: hashed, role: defaultRole });
+    console.log(newUser);
     res.status(201).json({ message: 'Registered successfully' });
   } catch (err) {
     res.status(500).json({ message: err.message });
